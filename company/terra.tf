ecd1392f2eaa1d5fa8c855b9e8b457ec750fd38e
vinni06 <58682029+vinni06@users.noreply.github.com>
2020-01-03 18:36:21 +0530

    Update terra.tf

--- company/terra.tf
+++ company/terra.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-        access_key= "AKIAXLC4ZCGUDPFOKE32"
-        secret_key="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"
-        region="eu-west-3"
+        access_key = "AKIAXLC4ZCGUJDX2IXWU"
+        secret_key = "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"
+        region     = "eu-west-3"
 }
-resource "aws_instance" "challengeDemo"{
-        ami="ami-0bb607148d8cf36fb"
-        instance_type="t2.micro"
+
+resource "aws_instance" "exampleDemo" {
+        ami             = "ami-0bb607148d8cf36fb"
+        instance_type   = "t2.micro"
 }
